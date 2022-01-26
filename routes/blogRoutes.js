6420f8bc454dca33f07a6087b79e3059dc998e76
Rochelle Maxwell <rochellemaxwell@Rochelles-MacBook-Air.local>
2022-01-26 11:29:02 -0500

    update routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -12,40 +12,36 @@ blogRouter.get('/',(req,res)=>{
 
 blogRouter.get('/:username',(req,res)=>{
     let username = req.body.username
+        res.status().json({message:})
 
-User.create(user,(error,result)=>{
-    if(error){
-            res.status(400).json({message:error.message})
-        }
-         res.status(200).json({message:"User OK",user:user})
     })
 blogRouter.post('/blog',(req,res)=>{
         let blog =req.body
+    }) 
+// blogRouter.get('/:id',(req,res)=>{
+//     let id = Number(req.params.id)
+//     if (id>=data.length || id < 0){
+//         res.status(404).json({message:"User not found"})
+//     }
+//         res.status(200).json({blog:data[id]})
+// })
 
-blogRouter.get('/:id',(req,res)=>{
-    let id = Number(req.params.id)
-    if (id>=data.length || id < 0){
-        res.status(404).json({message:"User not found"})
-    }
-        res.status(200).json({blog:data[id]})
-})
+// blogRouter.put('/:id',jwt.authenticationToken,(req,res)=>{
+//         let id = req.params.id
+//         Blog.findByIDANDUpdate(id,newBlog,(error,blog)=>{
+//             if(err){
+//                 res.status(400).json({message:error.message})
+//             }
+//             res.status(200).json({message:(error.message)})    
+//         }
+//     )
+//     })
 
-blogRouter.put('/:id',jwt.authenticationToken,(req,res)=>{
-        let id = req.params.id
-        Blog.findByIDANDUpdate(id,newBlog,(error,blog)=>{
-            if(err){
-                res.status(400).json({message:error.message})
-            }
-            res.status(200).json({message:(error.message)})    
-        }
-    )
-    })
-
-blogRouter.delete('/:id',(req,res)=>{
-  let id = req.params.id
-    res.status(404).json({message:"Why are you deleting me"})
-})
-        res.status(204).json({message:"Deleted"})
-    })
-})  
+// blogRouter.delete('/:id',(req,res)=>{
+//   let id = req.params.id
+//     res.status(404).json({message:"Why are you deleting me"})
+// })
+//         res.status(204).json({message:"Deleted"})
+//     })
+ 
 module.exports = blogRouter
